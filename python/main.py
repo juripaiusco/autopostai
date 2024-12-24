ade3302e862955d18f6d9d91a629a0644485a9b7
Juri <juri@mr-j.it>
2024-12-24 10:17:52 +0100

    Update query, prompt and img path

--- python/main.py
+++ python/main.py
@@ -1,37 +1,67 @@
+import os
+from dotenv import load_dotenv
 from lib.gpt import GPT
 from lib.meta import Meta
 from lib.mysql import Mysql
 
+load_dotenv(dotenv_path=".laravel-env")
 
 def main():
+    # prompt = "Crea un testo di prova super semplice e super breve"
+    # gpt = GPT()
+    # contenuto = gpt.generate(prompt)
+    # meta = Meta()
+    # meta.fb_generate_post(contenuto)
 
-    print("Hello World")
+    mysql = Mysql()
+    mysql.connect()
+    rows = mysql.query("""
+            SELECT  autopostai_posts.id AS id,
+                    autopostai_posts.user_id AS user_id,
+                    autopostai_posts.prompt AS prompt,
+                    autopostai_posts.img AS img,
+                    autopostai_posts.meta_facebook AS meta_facebook,
+                    autopostai_posts.meta_instagram AS meta_instagram,
+                    autopostai_posts.wordpress AS wordpress,
+                    autopostai_posts.newsletter AS newsletter,
+                    autopostai_posts.published_at AS published_at,
+                    autopostai_posts.published AS published,
+                    autopostai_settings.ai_personality AS ai_personality,
+                    autopostai_settings.ai_prompt_prefix AS ai_prompt_prefix,
+                    autopostai_settings.openai_api_key AS openai_api_key,
+                    autopostai_settings.meta_page_id AS meta_page_id
 
-  # prompt = "Crea un testo di prova super semplice e super breve"
-  # gpt = GPT()
-  # contenuto = gpt.generate(prompt)
-  # meta = Meta()
-  # meta.fb_generate_post(contenuto)
+                FROM autopostai_posts
+                INNER JOIN autopostai_settings ON autopostai_settings.user_id = autopostai_posts.user_id
 
-  # prompt = ("Racconta una storia basandoti su questra immagine, la storia deve includere elementi legati al"
-  #           "cosmo e i chakra, di come l'uomo sia un tuttuno con l'universo e di come queste pietre riescano a"
-  #           "donarti serenità e gratitudine. La storia è per un post Instagram e deve avere come call to action"
-  #           "il commentare le proprie esperienze per generare interazioni")
-  # img_path = "./storage/bracciale.jpg"
-  #
-  # gpt = GPT()
-  # contenuto = gpt.generate(prompt, img_path)
+            WHERE autopostai_posts.published IS NULL
+        """)
+    mysql.close()
 
-  # meta = Meta()
-  # meta.fb_generate_post(contenuto, img_path)
+    for row in rows:
+        prompt = ""
 
-  # meta = Meta()
-  # meta.ig_generate_post(contenuto, "https://.../bracciale.jpg")
+        if row['ai_personality']:
+            prompt = prompt + f"{row['ai_personality']}\n\n"
+
+        if row['ai_prompt_prefix']:
+            prompt = prompt + f"{row['ai_prompt_prefix']}\n\n"
+
+        if row['prompt']:
+            prompt = prompt + "#Guidelines\n\n"
+            prompt = prompt + f"{row['prompt']}"
+
+        img_url = f"{os.getenv('APP_URL')}/storage/posts/{row['id']}/{row['img']}"
+
+        # gpt = GPT()
+        # contenuto = gpt.generate(prompt, img_path)
+
+        # meta = Meta()
+        # meta.fb_generate_post(contenuto, img_path)
+
+        # meta = Meta()
+        # meta.ig_generate_post(contenuto, "https://.../bracciale.jpg")
 
 
 if __name__ == "__main__":
-  # main()
-    mysql = Mysql()
-    mysql.connect()
-    mysql.query("SELECT * FROM autopostai_posts WHERE published IS NULL")
-    mysql.close()
+    main()
