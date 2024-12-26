eec27ae18e02e83c2c676484e4b62787f5fe13bb
Juri <juri@mr-j.it>
2024-12-26 10:19:09 +0100

    Fix fields and control image settings

--- python/main.py
+++ python/main.py
@@ -7,23 +7,20 @@ from lib.mysql import Mysql
 load_dotenv(dotenv_path=".laravel-env")
 
 def main():
-    # prompt = "Crea un testo di prova super semplice e super breve"
-    # gpt = GPT()
-    # contenuto = gpt.generate(prompt)
-    # meta = Meta()
-    # meta.fb_generate_post(contenuto)
 
+    # Recupero i dati dal database per generare i Post
     mysql = Mysql()
     mysql.connect()
     rows = mysql.query("""
             SELECT  autopostai_posts.id AS id,
                     autopostai_posts.user_id AS user_id,
-                    autopostai_posts.prompt AS prompt,
+                    autopostai_posts.ai_prompt_post AS prompt,
                     autopostai_posts.img AS img,
-                    autopostai_posts.meta_facebook AS meta_facebook,
-                    autopostai_posts.meta_instagram AS meta_instagram,
-                    autopostai_posts.wordpress AS wordpress,
-                    autopostai_posts.newsletter AS newsletter,
+                    autopostai_posts.img_ai_check_on AS img_ai_check_on,
+                    autopostai_posts.meta_facebook_on AS meta_facebook,
+                    autopostai_posts.meta_instagram_on AS meta_instagram,
+                    autopostai_posts.wordpress_on AS wordpress,
+                    autopostai_posts.newsletter_on AS newsletter,
                     autopostai_posts.published_at AS published_at,
                     autopostai_posts.published AS published,
                     autopostai_settings.ai_personality AS ai_personality,
@@ -38,7 +35,10 @@ def main():
         """)
     mysql.close()
 
+    # Leggo tutti i post
     for row in rows:
+
+        # Creo il prompt
         prompt = ""
 
         if row['ai_personality']:
@@ -47,21 +47,39 @@ def main():
         if row['ai_prompt_prefix']:
             prompt = prompt + f"{row['ai_prompt_prefix']}\n\n"
 
-        if row['prompt']:
+        if row['ai_prompt_post']:
             prompt = prompt + "#Guidelines\n\n"
-            prompt = prompt + f"{row['prompt']}"
+            prompt = prompt + f"{row['ai_prompt_post']}"
 
+        # Definisco i percorsi delle immagini
         img_path = f"./storage/app/public/posts/{row['id']}/{row['img']}"
         img_url = f"{os.getenv('APP_URL')}/storage/posts/{row['id']}/{row['img']}"
 
+        # Classe OpenAI
         gpt = GPT(api_key=row['openai_api_key'])
-        contenuto = gpt.generate(prompt, img_path)
 
-        meta = Meta(row['meta_page_id'])
-        meta.fb_generate_post(contenuto, img_path)
+        # Classe Meta
+        meta = Meta(page_id=row['meta_page_id'])
 
-        # meta = Meta()
-        # meta.ig_generate_post(contenuto, "https://.../bracciale.jpg")
+        # Verifico se l'immagine è da inviare all'AI e se l'immagine esiste
+        if row['img_ai_check_on'] and row['img']:
+            contenuto = gpt.generate(prompt, img_path)
+        else:
+            contenuto = gpt.generate(prompt)
+
+        # Verifico se l'immagine è stata caricata e la invio ai canali scelti
+        if row['img']:
+
+            if row['meta_facebook_on']:
+                meta.fb_generate_post(contenuto, img_path)
+
+            if row['meta_instagram_on']:
+                meta.ig_generate_post(contenuto, img_url)
+
+        else:
+
+            if row['meta_facebook_on']:
+                meta.fb_generate_post(contenuto)
 
 
 if __name__ == "__main__":
