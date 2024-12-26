16ac0ac9e13acf7444ed0c44765abe1b386e5f7a
Juri <juri@mr-j.it>
2024-12-26 18:21:00 +0100

    create network with ip to connect to specific mysql server

--- python/autopostai.sh
+++ python/autopostai.sh
@@ -2,9 +2,18 @@
 
 #docker build -t autopostai .
 
+docker network create \
+  --subnet=192.168.1.0/24 \
+  autopostai_network
+
 docker run \
   --rm \
+  --net autopostai_network \
+  --ip 192.168.1.100 \
   --env-file .env \
   -v "$(pwd)/../.env":/app/.laravel-env \
   -v "$(pwd)/../storage":/app/storage \
   -v "$(pwd)":/app autopostai
+
+docker network rm \
+  autopostai_network
