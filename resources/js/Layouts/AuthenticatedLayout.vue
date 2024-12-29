e53fc8ea924daf5e13165cb1b9de34663924599e
Juri <juri@mr-j.it>
2024-12-29 09:02:46 +0100

    Automatic manager string

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -75,6 +75,9 @@ const showingNavigationDropdown = ref(false);
                                                 class="inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none dark:bg-gray-800 dark:text-gray-400 dark:hover:text-gray-300"
                                             >
                                                 {{ $page.props.auth.user.name }}
+                                                <span v-if="$page.props.auth.user.child_on">
+                                                    &nbsp;-&nbsp;Manager
+                                                </span>
 
                                                 <svg
                                                     class="-me-0.5 ms-2 h-4 w-4"
