6fc1e75ba176c05e103ae7bb64a06d763d29e84c
Juri <juri@mr-j.it>
2025-01-12 10:45:33 +0100

    API install

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/posts', function () {
+    return App\Models\Post::latest()->get();
+})/*->middleware('auth:sanctum')*/;
