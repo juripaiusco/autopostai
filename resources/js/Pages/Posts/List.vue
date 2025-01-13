cee9e3d9df62f357e7606d8b105933eae08dd3ec
Juri <juri@mr-j.it>
2025-01-13 09:23:23 +0100

    Fix polling post and change btn color

--- resources/js/Pages/Posts/List.vue
+++ resources/js/Pages/Posts/List.vue
@@ -225,13 +225,13 @@ let app_url = import.meta.env.VITE_APP_URL;
 
                                 if (d.published === '1') {
 
-                                    html += '<div class=\'btn btn-info\'>';
+                                    html += '<div class=\'btn btn-success\'>';
                                     html += '<svg class=\'w-4 h-4 xmlns=\'http://www.w3.org/2000/svg\' fill=\'none\' viewBox=\'0 0 24 24\' stroke-width=\'1.5\' stroke=\'currentColor\'><path stroke-linecap=\'round\' stroke-linejoin=\'round\' d=\'M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z\'/></svg>'
                                     html += '</div>';
 
                                 } else {
 
-                                    html += '<div class=\'btn btn-dark\'>';
+                                    html += '<div class=\'btn btn-warning\'>';
                                     html += '<svg class=\'w-4 h-4 xmlns=\'http://www.w3.org/2000/svg\' fill=\'none\' viewBox=\'0 0 24 24\' stroke-width=\'1.5\' stroke=\'currentColor\'><path stroke-linecap=\'round\' stroke-linejoin=\'round\' d=\'M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10\'/></svg>'
                                     html += '</div>';
                                 }
@@ -363,7 +363,7 @@ export default {
         };
 
         onMounted(() => {
-            fetchPosts(); // Caricamento iniziale
+            //fetchPosts(); // Caricamento iniziale
             interval = setInterval(fetchPosts, 30000); // 30 secondi
         });
 
