6647ba1d767c91a377fe49c64a446d61b4f25670
Juri <juri@mr-j.it>
2025-01-14 17:55:17 +0100

    Add replied field and docs fields

--- database/migrations/2024_12_22_163047_create_posts_table.php
+++ database/migrations/2024_12_22_163047_create_posts_table.php
@@ -19,14 +19,39 @@ return new class extends Migration
             // Relazione e comportamento in cascata
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 
+            // Titolo del post - uso interno
             $table->string('title');
+
+            // Istruzioni da inviare al LLM
             $table->longText('ai_prompt_post')->default('')->nullable();
+
+            // Risposta del LLM
             $table->longText('ai_content')->default('')->nullable();
+
+            // Immagine del post
             $table->string('img')->default('')->nullable();
+
+            // Check per dire al LLM se leggere o meno l'immagine
             $table->string('img_ai_check_on', 1)->default(0)->nullable();
+
+            // Lista dei canali di comunicazione, questa lista è dettata dall'utente
+            // se all'utente vengono cambiate impostazioni questo campo rimane invariato,
+            // ogni post potrà così avere opzioni capillari, cioè un post può ricevere risposte
+            // un altro post ha le risposte chiuse.
             $table->json('channels')->nullable();
+
+            // Data pubblicazione del post
             $table->timestamp('published_at')->nullable();
+
+            // Flag se il post è pubblicato
             $table->string('published', 1)->default(0)->nullable();
+
+            // Flag se il post ha concluso le risposte, nel campo channel vengono impostate le opzioni
+            // del post, una di queste è la replica e il numero di repliche, che poi vengono inserite
+            // nella tabella dei commenti. Una volta raggiunto il numero di risposte indicate il post
+            // si può considerare "replied", cioè ha risposto al numero massimo di commenti, così non
+            // verrà più esaminato dallo script Python e non farà più chiamate API ai vari canali.
+            $table->string('replied', 1)->default(0)->nullable();
 
             $table->timestamps();
         });
