4f166d756c80240bf8bd8453364d4aa868dfe957
Juri <juri@mr-j.it>
2025-01-15 21:46:27 +0100

    Change code position

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,6 +14,31 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasApiTokens, HasFactory, Notifiable;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'parent_id',
+        'child_on',
+        'child_max',
+        'tokens_limit',
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+//        'password',
+        'remember_token',
+    ];
+
     protected static function booted()
     {
         static::deleting(function ($user) {
@@ -47,30 +72,10 @@ class User extends Authenticatable
         return $this->hasOne(Settings::class, 'user_id', 'id');
     }
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
-        'parent_id',
-        'child_on',
-        'child_max',
-        'tokens_limit',
-        'name',
-        'email',
-        'password',
-    ];
+    public function tokens_used()
+    {
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-//        'password',
-        'remember_token',
-    ];
+    }
 
     /**
      * Get the attributes that should be cast.
