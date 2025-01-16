1a22bdd8fa16fd2182e688a5be546b52ae78db5d
Juri <juri@mr-j.it>
2025-01-16 09:11:01 +0100

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -93,5 +93,80 @@ class DatabaseSeeder extends Seeder
         Settings::factory()->create([
             'user_id' => $user->id
         ]);
+
+        // Insert Manager --------------------------------------
+
+        $channels = (new Users())->get_channels();
+
+        $manager = User::factory()->create([
+            'parent_id' => $admin->id,
+            'name' => 'Pietro',
+            'email' => 'pietro@prova.it',
+            'password' => Hash::make('12345'),
+            'child_on' => 1,
+            'child_max' => 2,
+        ]);
+
+        Settings::factory()->create([
+            'user_id' => $manager->id
+        ]);
+
+            // Insert Sub User --------------------------------------
+
+        $channels = (new Users())->get_channels();
+        $channels['facebook']['on'] = '1';
+        $channels['instagram']['on'] = '1';
+
+        $array_subuser = [
+            [
+                'name' => 'Bepi',
+                'email' => 'bepi@prova.it',
+            ], [
+                'name' => 'Toni',
+                'email' => 'toni@prova.it',
+            ]
+        ];
+        foreach ($array_subuser as $item) {
+
+            $user = User::factory()->create([
+                'parent_id' => $manager->id,
+                'name' => $item['name'],
+                'email' => $item['email'],
+                'password' => Hash::make('12345'),
+                'channels' => json_encode($channels),
+                'tokens_limit' => 10000
+            ]);
+
+            Settings::factory()->create([
+                'user_id' => $user->id
+            ]);
+
+            $posts = Post::factory(2)->create([
+                'user_id' => $user->id,
+                'channels' => json_encode($channels)
+            ]);
+
+            foreach ($posts as $post) {
+                Token_log::factory()->create([
+                    'user_id' => $user->id,
+                    'type' => 'post',
+                    'reference_id' => $post->id,
+                    'tokens_used' => rand(50, 1000),
+                ]);
+
+                $comments = Comment::factory(3)->create([
+                    'post_id' => $post->id
+                ]);
+
+                foreach ($comments as $comment) {
+                    Token_log::factory()->create([
+                        'user_id' => $user->id,
+                        'type' => 'comment',
+                        'reference_id' => $comment->id,
+                        'tokens_used' => rand(50, 200),
+                    ]);
+                }
+            }
+        }
     }
 }
