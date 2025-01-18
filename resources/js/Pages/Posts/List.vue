530791672e39f5d45a486761d6b27bcab28bf183
Juri <juri@mr-j.it>
2025-01-18 11:59:44 +0100

    task_complete gray btn

--- resources/js/Pages/Posts/List.vue
+++ resources/js/Pages/Posts/List.vue
@@ -252,7 +252,13 @@ let app_url = import.meta.env.VITE_APP_URL;
 
                                 if (d.published === '1') {
 
-                                    html += '<div class=\'btn btn-success\'>';
+                                    let className = 'btn-success';
+
+                                    if (d.task_complete === '1') {
+                                        className = 'btn-secondary';
+                                    }
+
+                                    html += '<div class=\'btn ' + className + '\'>';
                                     html += '<svg class=\'w-4 h-4 xmlns=\'http://www.w3.org/2000/svg\' fill=\'none\' viewBox=\'0 0 24 24\' stroke-width=\'1.5\' stroke=\'currentColor\'><path stroke-linecap=\'round\' stroke-linejoin=\'round\' d=\'M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z\'/></svg>'
                                     html += '</div>';
 
