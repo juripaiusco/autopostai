bfc0c5e04e143db07a5cbb93397658de2c399044
Juri <juri@mr-j.it>
2025-01-25 18:16:50 +0100

    Fix mobile layout login

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -5,20 +5,20 @@ import { Link } from '@inertiajs/vue3';
 
 <template>
     <div
-        class="min-h-screen flex flex-col items-center pt-[50px] bg-gray-100 dark:bg-gray-900"
+        class="min-h-screen flex flex-col items-center pt-[50px] sm:bg-gray-100 dark:bg-gray-900"
     >
         <div>
             <Link href="/">
                 <ApplicationLogo class="w-20 h-20 fill-current text-gray-500 m-auto" />
                 <br>
                 <h1 class="text-3xl mb-[20px] dark:text-white">
-                    AutoPost<span class="font-bold">AI</span> <small class="text-sm">v.1</small>
+                    AutoPost<span class="font-bold">AI</span> <small class="text-sm">v.1.0.0</small>
                 </h1>
             </Link>
         </div>
 
         <div
-            class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg"
+            class="w-full sm:max-w-md mt-6 px-6 py-4 sm:bg-white sm:dark:bg-gray-800 sm:shadow-md overflow-hidden sm:rounded-lg"
         >
             <slot />
         </div>
