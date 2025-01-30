146f5508b1ccb2aebc02f18eaebeba73d0346834
Juri <juri@mr-j.it>
2025-01-30 21:04:06 +0100

    fix mobile bottom nav menu

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -257,7 +257,8 @@ const showingNavigationDropdown = ref(false);
             </main>
         </div>
 
-        <nav class="fixed bottom-[-2px] w-full py-2 bg-gray-100 text-gray-400 dark:bg-gray-900 dark:text-gray-600 sm:hidden">
+        <!-- Movile botton Nav -->
+        <nav class="fixed bottom-[-2px] w-full py-2 pb-8 bg-gray-100 text-gray-400 dark:bg-gray-900 dark:text-gray-600 sm:hidden">
             <div class="container mx-auto flex justify-around">
 
                 <Link v-if="!$page.props.auth.user.parent_id || $page.props.auth.user.child_on"
