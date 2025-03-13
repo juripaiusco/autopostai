b5b77751893592cc88b2817afe676949c5a7cd63
Juri <juri@mr-j.it>
2025-03-13 19:07:31 +0100

    Layout button preview

--- resources/js/Pages/Posts/Form.vue
+++ resources/js/Pages/Posts/Form.vue
@@ -528,27 +528,33 @@ function previewAIContent() {
 
                 </div>
 
-                <div class="text-right mt-10 flex flex-wrap justify-center md:justify-end">
+                <div class="text-right mt-10 mb-2 flex flex-wrap justify-center md:justify-end">
 
-                    <div class="w-[100%] text-center md:w-auto mb-2 sm:hidden">
+                    <div class="text-center w-[100%] md:w-auto">
                         <button :disabled="!form.id && !form.user_id"
                                 type="button"
-                                class="btn btn-primary w-[100%] md:w-[120px]"
-                                @click="previewAIContent">Anteprima&nbsp;AI</button>
+                                class="btn btn-primary w-[100%] md:w-[248px] !flex items-center justify-center gap-2"
+                                @click="previewAIContent">
+                            <span class="inline-flex">
+                                Anteprima
+                            </span>
+                            <span class="inline-flex shrink-0">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z" />
+                                </svg>
+                            </span>
+                        </button>
                     </div>
+
+                </div>
+
+                <div class="text-right flex flex-wrap justify-center md:justify-end">
 
                     <div class="w-1/2 text-center md:w-auto pr-2">
                         <Link class="btn btn-secondary w-[100%] md:w-[120px]"
                               :href="data.saveRedirect">
                             Annulla
                         </Link>
-                    </div>
-
-                    <div class="text-center md:w-auto pr-2 hidden sm:block">
-                        <button :disabled="!form.id && !form.user_id"
-                                type="button"
-                                class="btn btn-primary w-[100%] md:w-[120px]"
-                                @click="previewAIContent">Anteprima&nbsp;AI</button>
                     </div>
 
                     <div class="w-1/2 text-center md:w-auto">
