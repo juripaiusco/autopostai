47d83eee1334bc65e5939f1c00132cfb4ee966ec
Juri <juri@mr-j.it>
2025-07-18 12:08:34 +0200

    Add WordPress class to manage WordPress connections

---
+++ docker/autopostai/services/wordpress.py
@@ -0,0 +1,77 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+class Wordpress:
+    def __init__(self, wordpress_url, wordpress_username, wordpress_password):
+        self.wordpress_url = wordpress_url
+        self.wordpress_username = wordpress_username
+        self.wordpress_password = wordpress_password
+
+    def auth(self):
+        if self.wordpress_url is not None:
+            return HTTPBasicAuth(self.wordpress_username, self.wordpress_password)
+
+    def send(self, title, content, img_path, cat_id):
+        data = {
+            "title": title,
+            "content": content,
+            "status": "publish",
+            "categories": [cat_id] if cat_id else []
+        }
+
+        response = requests.post(
+            f"{self.wordpress_url}/wp-json/wp/v2/posts",
+            auth=self.auth(),
+            json=data
+        )
+
+        post_id = response.json().get("id")
+        post_url = response.json().get("link")
+
+        # Upload immagine
+        if img_path:
+            # URL API per caricare l'immagine
+            url = f"{self.wordpress_url}/wp-json/wp/v2/media"
+
+            # Aprire il file e inviare la richiesta
+            with open(img_path, "rb") as img:
+                files = {"file": img}
+                headers = {"Content-Disposition": f"attachment; filename={img_path}"}
+                response = requests.post(url, auth=self.auth(), files=files, headers=headers)
+
+            # Ottenere l'ID dell'immagine caricata
+            image_id = response.json().get("id")
+
+            # URL per aggiornare il post
+            url = f"{self.wordpress_url}/wp-json/wp/v2/posts/{post_id}"
+
+            # Dati per assegnare l'immagine come immagine in evidenza
+            data = {"featured_media": image_id}
+
+            # Invia la richiesta PATCH per aggiornare il post
+            response = requests.post(url, auth=self.auth(), json=data)
+
+        return post_id, post_url
+
+    def update(self, post_id, title, content):
+        data = {
+            "title": title,
+            "content": content,
+            "status": "publish"
+        }
+
+        response = requests.post(
+            f"{self.wordpress_url}/wp-json/wp/v2/posts/{post_id}",
+            auth=self.auth(),
+            json=data
+        )
+
+        return response.json().get("id")
+
+    def delete(self, post_id):
+        response = requests.delete(
+            f"{self.wordpress_url}/wp-json/wp/v2/posts/{post_id}",
+            auth=self.auth()
+        )
+
+        return response.json().get("id")
--- docker/autopostai/task/posts/wordpress.py
+++ docker/autopostai/task/posts/wordpress.py
@@ -5,6 +5,7 @@ from typing import List
 import requests
 from requests.auth import HTTPBasicAuth
 import markdown
+from services.wordpress import Wordpress
 
 class WordPressPost(BasePost):
     def __init__(self, data: List[any] = None, debug = False):
@@ -30,92 +31,130 @@ class WordPressPost(BasePost):
 
         return title, body_html
 
+    def wordpress_init(self):
+        return Wordpress(
+            wordpress_url=self.data['wordpress_url'],
+            wordpress_username=self.data['wordpress_username'],
+            wordpress_password=self.data['wordpress_password']
+        )
+
     def send(self, content):
-        if self.data['wordpress_url'] is not None:
-            auth = HTTPBasicAuth(self.data['wordpress_username'], self.data['wordpress_password'])
+        title, body = self.get_data(content)
+        cat_id = self.data.get('wordpress_cat_id')
 
-            title, body = self.get_data(content)
-            cat_id = self.data.get('wordpress_cat_id')
+        # Assicuriamoci che cat_id sia un numero valido
+        if not isinstance(cat_id, int):
+            print(f"⚠️ Errore: ID categoria non valido ({cat_id})")
+            cat_id = None
 
-            # Assicuriamoci che cat_id sia un numero valido
-            if not isinstance(cat_id, int):
-                print(f"⚠️ Errore: ID categoria non valido ({cat_id})")
-                cat_id = None
+        post_id, post_url = self.wordpress_init().send(
+            title=title,
+            content=body,
+            img_path=self.img_path_get() if self.data['img'] else None,
+            cat_id=cat_id,
+        )
 
-            data = {
-                "title": title,
-                "content": body,
-                "status": "publish",
-                "categories": [cat_id] if cat_id else []
-            }
+        if self.debug:
+            print(datetime.now(cfg.LOCAL_TIMEZONE).strftime('%Y-%m-%d %H:%M:%S'), "WordPress - post ID:",
+                  post_id)
 
-            response = requests.post(
-                f"{self.data['wordpress_url']}/wp-json/wp/v2/posts",
-                auth=auth,
-                json=data
-            )
+        return post_id, post_url
 
-            post_id = response.json().get("id")
-            post_url = response.json().get("link")
-
-            # Upload immagine
-            if self.data['img']:
-                # Percorso del file da caricare
-                file_path = self.img_path_get()
-
-                # URL API per caricare l'immagine
-                url = f"{self.data['wordpress_url']}/wp-json/wp/v2/media"
-
-                # Aprire il file e inviare la richiesta
-                with open(file_path, "rb") as img:
-                    files = {"file": img}
-                    headers = {"Content-Disposition": f"attachment; filename={file_path}"}
-                    response = requests.post(url, auth=auth, files=files, headers=headers)
-
-                # Ottenere l'ID dell'immagine caricata
-                image_id = response.json().get("id")
-
-                # URL per aggiornare il post
-                url = f"{self.data['wordpress_url']}/wp-json/wp/v2/posts/{post_id}"
-
-                # Dati per assegnare l'immagine come immagine in evidenza
-                data = {"featured_media": image_id}
-
-                # Invia la richiesta PATCH per aggiornare il post
-                response = requests.post(url, auth=auth, json=data)
-
-            if self.debug:
-                print(datetime.now(cfg.LOCAL_TIMEZONE).strftime('%Y-%m-%d %H:%M:%S'), "WordPress - post ID:", response.json().get("id"))
-
-            return post_id, post_url
+        # if self.data['wordpress_url'] is not None:
+        #     auth = HTTPBasicAuth(self.data['wordpress_username'], self.data['wordpress_password'])
+        #
+        #     title, body = self.get_data(content)
+        #     cat_id = self.data.get('wordpress_cat_id')
+        #
+        #     # Assicuriamoci che cat_id sia un numero valido
+        #     if not isinstance(cat_id, int):
+        #         print(f"⚠️ Errore: ID categoria non valido ({cat_id})")
+        #         cat_id = None
+        #
+        #     data = {
+        #         "title": title,
+        #         "content": body,
+        #         "status": "publish",
+        #         "categories": [cat_id] if cat_id else []
+        #     }
+        #
+        #     response = requests.post(
+        #         f"{self.data['wordpress_url']}/wp-json/wp/v2/posts",
+        #         auth=auth,
+        #         json=data
+        #     )
+        #
+        #     post_id = response.json().get("id")
+        #     post_url = response.json().get("link")
+        #
+        #     # Upload immagine
+        #     if self.data['img']:
+        #         # Percorso del file da caricare
+        #         file_path = self.img_path_get()
+        #
+        #         # URL API per caricare l'immagine
+        #         url = f"{self.data['wordpress_url']}/wp-json/wp/v2/media"
+        #
+        #         # Aprire il file e inviare la richiesta
+        #         with open(file_path, "rb") as img:
+        #             files = {"file": img}
+        #             headers = {"Content-Disposition": f"attachment; filename={file_path}"}
+        #             response = requests.post(url, auth=auth, files=files, headers=headers)
+        #
+        #         # Ottenere l'ID dell'immagine caricata
+        #         image_id = response.json().get("id")
+        #
+        #         # URL per aggiornare il post
+        #         url = f"{self.data['wordpress_url']}/wp-json/wp/v2/posts/{post_id}"
+        #
+        #         # Dati per assegnare l'immagine come immagine in evidenza
+        #         data = {"featured_media": image_id}
+        #
+        #         # Invia la richiesta PATCH per aggiornare il post
+        #         response = requests.post(url, auth=auth, json=data)
+        #
+        #     if self.debug:
+        #         print(datetime.now(cfg.LOCAL_TIMEZONE).strftime('%Y-%m-%d %H:%M:%S'), "WordPress - post ID:", response.json().get("id"))
+        #
+        #     return post_id, post_url
 
     def update(self, post_id, content):
-        if self.data['wordpress_url'] is not None:
-            auth = HTTPBasicAuth(self.data['wordpress_username'], self.data['wordpress_password'])
+        title, body = self.get_data(content)
 
-            title, body = self.get_data(content)
+        return self.wordpress_init().update(
+            post_id=post_id,
+            title=title,
+            content=body,
+        )
 
-            data = {
-                "title": title,
-                "content": body,
-                "status": "publish"
-            }
-
-            response = requests.patch(
-                f"{self.data['wordpress_url']}/wp-json/wp/v2/posts/{post_id}",
-                auth=auth,
-                json=data
-            )
-
-            return response.json().get("id")
+        # if self.data['wordpress_url'] is not None:
+        #     auth = HTTPBasicAuth(self.data['wordpress_username'], self.data['wordpress_password'])
+        #
+        #     title, body = self.get_data(content)
+        #
+        #     data = {
+        #         "title": title,
+        #         "content": body,
+        #         "status": "publish"
+        #     }
+        #
+        #     response = requests.patch(
+        #         f"{self.data['wordpress_url']}/wp-json/wp/v2/posts/{post_id}",
+        #         auth=auth,
+        #         json=data
+        #     )
+        #
+        #     return response.json().get("id")
 
     def delete(self, post_id):
-        if self.data['wordpress_url'] is not None:
-            auth = HTTPBasicAuth(self.data['wordpress_username'], self.data['wordpress_password'])
+        return self.wordpress_init().delete(post_id)
 
-            response = requests.delete(
-                f"{self.data['wordpress_url']}/wp-json/wp/v2/posts/{post_id}",
-                auth=auth
-            )
-
-            return response.json().get("id")
+        # if self.data['wordpress_url'] is not None:
+        #     auth = HTTPBasicAuth(self.data['wordpress_username'], self.data['wordpress_password'])
+        #
+        #     response = requests.delete(
+        #         f"{self.data['wordpress_url']}/wp-json/wp/v2/posts/{post_id}",
+        #         auth=auth
+        #     )
+        #
+        #     return response.json().get("id")
