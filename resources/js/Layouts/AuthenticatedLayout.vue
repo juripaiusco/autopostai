f89ffd9f3ccb961ad37a7aa54a329a81a839bd58
Juri <juri@mr-j.it>
2025-08-08 14:18:57 +0200

    fix space

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -275,10 +275,8 @@ const showingNavigationDropdown = ref(false);
             </nav>
 
             <!-- Page Heading -->
-            <header
-                class="bg-white shadow dark:bg-gray-800"
-                v-if="$slots.header"
-            >
+            <header class="bg-white shadow dark:bg-gray-800"
+                    v-if="$slots.header">
                 <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                     <slot name="header" />
                 </div>
