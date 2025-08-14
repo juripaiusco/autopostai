e073ec9fc5a8a5ce5387b7bd73bf6a09a09d889b
Juri <juri@mr-j.it>
2025-08-14 15:07:45 +0200

    fix dark theme

--- resources/js/Pages/Notifications/List.vue
+++ resources/js/Pages/Notifications/List.vue
@@ -75,18 +75,24 @@ let modalData = ref(props.data);
                                 html += '</div>';
 
                                 if (d.body) {
-                                    html += '<div class=\'text-sm text-gray-700 mt-1\'>' + d.body + '</div>';
+                                    html += '<div class=\'text-sm text-gray-700 dark:text-gray-400 mt-1\'>' + d.body + '</div>';
                                 }
 
                                 if (d.url) {
-                                    html += '<div class=\'text-sm text-gray-500\'>';
+                                    html += '<div class=\'text-sm text-gray-500 dark:text-gray-300\'><small>';
                                     html += d.url;
-                                    html += '</div>';
+                                    html += '</small></div>';
                                 }
 
-                                html += '<div class=\'text-xs text-gray-500 mt-2\'>';
-                                html += __date(d.sent_at, 'day') + ' ' + __date(d.sent_at, 'hour');
-                                html += '</div>';
+                                if (d.sent_at !== null) {
+                                    html += '<div class=\'text-xs text-gray-500 mt-2\'>';
+                                    html += __date(d.sent_at, 'day') + ' ' + __date(d.sent_at, 'hour');
+                                    html += '</div>';
+                                } else {
+                                    html += '<div class=\'flex items-center mt-2\'>';
+                                    html += '<svg class=\'text-red-500 size-4\' xmlns=\'http://www.w3.org/2000/svg\' fill=\'none\' viewBox=\'0 0 24 24\' stroke-width=\'1.5\' stroke=\'currentColor\'><path stroke-linecap=\'round\' stroke-linejoin=\'round\' d=\'M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z\' /></svg>';
+                                    html += '</div>';
+                                }
 
                                 return html;
                             }
