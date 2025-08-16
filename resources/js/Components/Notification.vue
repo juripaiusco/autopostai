5b9490d02630ea8d85a8d9cb023247d3e1b48727
Juri <juri@mr-j.it>
2025-08-16 22:29:11 +0200

    set link icon on notifications list

--- resources/js/Components/Notification.vue
+++ resources/js/Components/Notification.vue
@@ -98,7 +98,21 @@ const {
                             :target="n.url ? '_blank' : ''"
                             class="block px-4 py-3 hover:bg-gray-50 transition"
                         >
-                            <p class="font-medium text-sm text-gray-800">{{ n.title }}</p>
+                            <div class="flex items-center font-medium text-sm text-gray-800">
+
+                                <span v-if="n.url"
+                                      class="mr-1 text-gray-400">
+
+                                    <svg class="size-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
+                                    </svg>
+
+                                </span>
+
+                                <span>
+                                    {{ n.title }}
+                                </span>
+                            </div>
                             <p class="text-gray-600 text-xs mb-1">{{ n.body }}</p>
                             <p class="text-gray-400 text-xs">
                                 {{ __date(n.sent_at, 'day') }} - {{ __date(n.sent_at, 'hour') }}
