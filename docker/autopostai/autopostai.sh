fbcb357ef20b8855dd46cb57ffd14e6d7c94e2c5
Juri <juri@mr-j.it>
2026-03-13 21:24:05 +0100

    ctrl if project is in deploy mode

--- docker/autopostai/autopostai.sh
+++ docker/autopostai/autopostai.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
 # Crontab
-# */5	*	*	*	*	/var/www/vhosts/server-j.com/autopostai.server-j.com/python/autopostai.sh >> /root/autopostai.log 2>&1
+# *   *   *   *   *   /var/www/vhosts/server-j.com/autopostai.server-j.com/docker/autopostai/autopostai.sh >> /root/autopostai.log 2>&1
 
 (
     PATH_SCRIPT=$(dirname "$0")
     cd $PATH_SCRIPT
-
-    # Carica il file .env e rende le variabili disponibili
-    set -a
-    source .env
-    set +a
-
-    export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
     echo ""
     echo ""
@@ -20,78 +13,94 @@
     echo "Script - START: $(date)"
     echo "------------------------------------------------------------"
 
-    IMAGE_NAME="autopostai"
-    CONTAINER_NAME=$IMAGE_NAME
-    NETWORK_NAME="autopostai_network"
-    SUBNET="192.168.1.0/24"
-    CONTAINER_IP="192.168.1.100"
-    DOCKERFILE="Dockerfile"
+    # Verifico se AutoPostAI è in aggiornamento
+    if [ ! -f "../../../marker_deploy" ]; then
 
-    # Funzione per controllare se l'immagine è aggiornata
-    needs_build() {
-      if [ ! -f ".docker_image_built" ]; then
-        # Primo build
-        return 0
-      fi
+        # Carica il file .env e rende le variabili disponibili
+        set -a
+        source .env
+        set +a
 
-      # Controlla la data di modifica del Dockerfile rispetto al file di stato
-      if [ "$DOCKERFILE" -nt ".docker_image_built" ]; then
-        return 0
-      fi
+        export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-      return 1
-    }
+        IMAGE_NAME="autopostai"
+        CONTAINER_NAME=$IMAGE_NAME
+        NETWORK_NAME="autopostai_network"
+        SUBNET="192.168.1.0/24"
+        CONTAINER_IP="192.168.1.100"
+        DOCKERFILE="Dockerfile"
 
-    # Compila l'immagine solo se necessario
-    if needs_build; then
-        echo "Costruzione dell'immagine Docker $IMAGE_NAME..."
-        docker build -t $IMAGE_NAME .
-        # Aggiorna il timestamp del file di stato
-        touch .docker_image_built
+        # Funzione per controllare se l'immagine è aggiornata
+        needs_build() {
+          if [ ! -f ".docker_image_built" ]; then
+            # Primo build
+            return 0
+          fi
 
-        # Se esiste già un container, fermalo ed eliminalo
-        if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
-            echo "Arresto ed eliminazione del vecchio container $CONTAINER_NAME..."
-            docker stop $CONTAINER_NAME
-            docker rm $CONTAINER_NAME
+          # Controlla la data di modifica del Dockerfile rispetto al file di stato
+          if [ "$DOCKERFILE" -nt ".docker_image_built" ]; then
+            return 0
+          fi
+
+          return 1
+        }
+
+        # Compila l'immagine solo se necessario
+        if needs_build; then
+            echo "Costruzione dell'immagine Docker $IMAGE_NAME..."
+            docker build -t $IMAGE_NAME .
+            # Aggiorna il timestamp del file di stato
+            touch .docker_image_built
+
+            # Se esiste già un container, fermalo ed eliminalo
+            if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
+                echo "Arresto ed eliminazione del vecchio container $CONTAINER_NAME..."
+                docker stop $CONTAINER_NAME
+                docker rm $CONTAINER_NAME
+            fi
+
+            # Avvia il nuovo container con restart always
+            echo "Avvio del nuovo container $CONTAINER_NAME..."
+            docker run -d \
+                --name $CONTAINER_NAME \
+                --restart always \
+                -p 8000:8000 \
+                --env-file .env \
+                -v "$(pwd)/../../.env":/app/.laravel-env \
+                -v "$(pwd)/../../storage":/app/storage \
+                -v "$(pwd)":/app \
+                $IMAGE_NAME \
+                uvicorn api:app --host 0.0.0.0 --port 8000
+        else
+            echo "L'immagine Docker $IMAGE_NAME è aggiornata."
         fi
 
-        # Avvia il nuovo container con restart always
-        echo "Avvio del nuovo container $CONTAINER_NAME..."
-        docker run -d \
-            --name $CONTAINER_NAME \
-            --restart always \
-            -p 8000:8000 \
-            --env-file .env \
-            -v "$(pwd)/../../.env":/app/.laravel-env \
-            -v "$(pwd)/../../storage":/app/storage \
-            -v "$(pwd)":/app \
-            $IMAGE_NAME \
-            uvicorn api:app --host 0.0.0.0 --port 8000
+        # Esegui il container
+        if [ "$1" == "" ]; then
+            # Crea la rete Docker, se non esiste
+            if ! docker network ls | grep -q $NETWORK_NAME; then
+              docker network create --subnet=$SUBNET $NETWORK_NAME
+            fi
+
+            docker run \
+                --rm \
+                --net $NETWORK_NAME \
+                --ip $CONTAINER_IP \
+                --env-file .env \
+                -v "$(pwd)/../../.env":/app/.laravel-env \
+                -v "$(pwd)/../../storage":/app/storage \
+                -v "$(pwd)":/app \
+                $IMAGE_NAME \
+                python main.py
+
+            # Rimuovi la rete Docker dopo l'esecuzione
+            docker network rm $NETWORK_NAME
+        fi
+
     else
-        echo "L'immagine Docker $IMAGE_NAME è aggiornata."
-    fi
 
-    # Esegui il container
-    if [ "$1" == "" ]; then
-        # Crea la rete Docker, se non esiste
-        if ! docker network ls | grep -q $NETWORK_NAME; then
-          docker network create --subnet=$SUBNET $NETWORK_NAME
-        fi
+        echo "Script - update: $(date)"
 
-        docker run \
-            --rm \
-            --net $NETWORK_NAME \
-            --ip $CONTAINER_IP \
-            --env-file .env \
-            -v "$(pwd)/../../.env":/app/.laravel-env \
-            -v "$(pwd)/../../storage":/app/storage \
-            -v "$(pwd)":/app \
-            $IMAGE_NAME \
-            python main.py
-
-        # Rimuovi la rete Docker dopo l'esecuzione
-        docker network rm $NETWORK_NAME
     fi
 
     echo "------------------------------------------------------------"
